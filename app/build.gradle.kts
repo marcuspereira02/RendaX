a4b932ed72a10fbdf15b6a4bcb05cde142804050
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-01-03 20:07:59 -0300

    lógica do app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.marcuspereira.appinvesty"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.marcuspereira.appinvesty"
